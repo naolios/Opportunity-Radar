03dac150e79d18870ae90a5c4f66fa51e5deceb4
naolios <naol.edesa.971@gmail.com>
2026-05-04 17:47:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,20 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // This must match your Express server PORT
+        changeOrigin: true,
+      }
+    }
+  }
+})
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
